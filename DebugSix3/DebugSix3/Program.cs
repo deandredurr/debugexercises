6c5e6dcb2738684cf6265f0828f8f9c0127a735e
Deandre Durr <deandre@swiftbloom.com>
2017-03-07 09:21:11 -0500

    New additons

---
+++ DebugSix3/DebugSix3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DebugSix3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] books = {"Catch-22", "Harry Potter",
+         "Programming Using C#", "Rich Dad, Poor Dad",
+         "The Deep", "Wizard of Oz"};
+            int x;
+            string entryString;
+            Write("What book are you looking for? ");
+            entryString = Console.ReadLine();
+            x = Array.BinarySearch(books, entryString);
+            if (x < 0)
+                WriteLine("{0} not found", entryString);
+            else
+                WriteLine("Yes, we carry {0}", entryString);
+        }
+    }
+}
